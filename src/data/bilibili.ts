cfe800b5ce3be07e2373fefb35a13ace76c2d162
Liangbandexiaojian <Liangbandexiaojian@users.noreply.github.com>
2026-08-20 00:00:12 +0800

    Add bilibili section and refresh homepage

---
+++ src/data/bilibili.ts
@@ -0,0 +1,389 @@
+export type BilibiliVideo = {
+  bvid: string;
+  title: string;
+  publishedAt: number;
+  duration: string;
+  play: number;
+  pic: string;
+};
+
+export const bilibiliSpaceUrl = 'https://space.bilibili.com/347880162';
+
+export const bilibiliVideos: BilibiliVideo[] = [
+  {
+    bvid: 'BV1NF411Z7Jz',
+    title: '在老友家中歌唱《毕业歌》',
+    publishedAt: 1691683114,
+    duration: '01:52',
+    play: 412,
+    pic: 'https://i1.hdslb.com/bfs/archive/3279d4ade27ac1a45466b215e59c68a691b6bced.jpg'
+  },
+  {
+    bvid: 'BV1sG4y1C7RX',
+    title: 'VLOG无锡嵩山→西仓古镇→宛山湖',
+    publishedAt: 1673694081,
+    duration: '04:23',
+    play: 902,
+    pic: 'https://i1.hdslb.com/bfs/archive/e342d7fd43f394b10156ed971e45f47e889c6c61.jpg'
+  },
+  {
+    bvid: 'BV1NN4y1g7Qo',
+    title: 'UP主高考查分时，竟然......',
+    publishedAt: 1656660074,
+    duration: '03:34',
+    play: 1518,
+    pic: 'https://i1.hdslb.com/bfs/archive/8d9d058a1ac5dc1d6f3955872738d4b503f1e147.jpg'
+  },
+  {
+    bvid: 'BV14h411s79b',
+    title: 'UP倾情演唱《黄河渔娘》',
+    publishedAt: 1638017565,
+    duration: '04:45',
+    play: 552,
+    pic: 'https://i0.hdslb.com/bfs/archive/5dc103568325dcd2cf813225074f80fe5a2fea7a.jpg'
+  },
+  {
+    bvid: 'BV1Q64y1a7vo',
+    title: '和妹妹一起拼中国空间站积木',
+    publishedAt: 1630209949,
+    duration: '03:11',
+    play: 335,
+    pic: 'https://i2.hdslb.com/bfs/archive/d5632bbfc1c77e76d6f90c5e4e0b47af1635cff0.jpg'
+  },
+  {
+    bvid: 'BV1j5411A7KK',
+    title: '英语朗读 星运里的错 The Fault in Our Stars',
+    publishedAt: 1617527329,
+    duration: '13:27',
+    play: 407,
+    pic: 'https://i1.hdslb.com/bfs/archive/f471f61333ed4b76c3c2ab7ad81fe88dbce08120.jpg'
+  },
+  {
+    bvid: 'BV1hK411G787',
+    title: '第一届天一中学传承杯足球赛 决赛完整视频 2020年 高二vs高三',
+    publishedAt: 1604155199,
+    duration: '51:55',
+    play: 885,
+    pic: 'https://i1.hdslb.com/bfs/archive/a3bbab1146309a9e4eb048b877d253fee281035d.jpg'
+  },
+  {
+    bvid: 'BV1ki4y1g7jm',
+    title: '小破车在城中一隅找到了奔跑的感觉',
+    publishedAt: 1597831970,
+    duration: '01:23',
+    play: 81,
+    pic: 'https://i1.hdslb.com/bfs/archive/18d8cf27d6bdc83c080286ba35c5f790457afe91.jpg'
+  },
+  {
+    bvid: 'BV1LC4y1b7AW',
+    title: 'UP主倾情演唱《葬花吟》',
+    publishedAt: 1595263816,
+    duration: '03:06',
+    play: 254,
+    pic: 'https://i1.hdslb.com/bfs/archive/ed485589fc543f94d5c61e020cdd1a2946c47d7e.jpg'
+  },
+  {
+    bvid: 'BV1dk4y1q7dA',
+    title: '惠山 梅雨季的春申涧',
+    publishedAt: 1594216019,
+    duration: '01:04',
+    play: 303,
+    pic: 'https://i2.hdslb.com/bfs/archive/8877e5105a0ac44623cd5a35f64b2f2ca3aa52c5.jpg'
+  },
+  {
+    bvid: 'BV1LT4y1E73v',
+    title: '2020外公外婆桃园',
+    publishedAt: 1594168957,
+    duration: '01:18',
+    play: 63,
+    pic: 'https://i1.hdslb.com/bfs/archive/4093dc66dd4e44676009c3522eab905b7e2334cd.jpg'
+  },
+  {
+    bvid: 'BV1tk4y1q7nx',
+    title: '2020天一中学高一足球班级联赛视频合集',
+    publishedAt: 1593108609,
+    duration: '54:41',
+    play: 13552,
+    pic: 'https://i1.hdslb.com/bfs/archive/d381835be94459b1273cf4f08f9cc0194d1e0845.jpg'
+  },
+  {
+    bvid: 'BV1pp4y197Hf',
+    title: '用初中物理来理解变焦和对焦',
+    publishedAt: 1588422614,
+    duration: '03:38',
+    play: 16864,
+    pic: 'https://i1.hdslb.com/bfs/archive/9d22075fbd0e3230210273eba7d2870caf622064.jpg'
+  },
+  {
+    bvid: 'BV16g4y1b7nE',
+    title: '用李子柒的方式打开中药香囊制作......',
+    publishedAt: 1585845472,
+    duration: '02:22',
+    play: 13506,
+    pic: 'https://i1.hdslb.com/bfs/archive/dd96e1e8fb89488bfd3355fed4241c4e9122c5eb.jpg'
+  },
+  {
+    bvid: 'BV1tV411f7eb',
+    title: '【翻译】教你如何制作硼处理魔方！boron treat cubes教程',
+    publishedAt: 1585456162,
+    duration: '07:29',
+    play: 565,
+    pic: 'https://i1.hdslb.com/bfs/archive/03bd83d19cb395af737e74ba8329bba1e2b9e9aa.jpg'
+  },
+  {
+    bvid: 'BV1K7411m7rB',
+    title: '春天到啦！',
+    publishedAt: 1585128408,
+    duration: '00:39',
+    play: 83,
+    pic: 'https://i1.hdslb.com/bfs/archive/792b2fcb598f38be1704005460a13669b6a9fe5d.jpg'
+  },
+  {
+    bvid: 'BV1BE411J7Qs',
+    title: '【翻译】cubicle硼处理魔方究竟是怎么一回事？',
+    publishedAt: 1582991700,
+    duration: '18:19',
+    play: 1305,
+    pic: 'https://i0.hdslb.com/bfs/archive/dc23908a417d1225a892d26a53110d81377d1d61.jpg'
+  },
+  {
+    bvid: 'BV1N7411r7hT',
+    title: '光謇在2019苏州赛上的精彩瞬间',
+    publishedAt: 1580130735,
+    duration: '00:56',
+    play: 236,
+    pic: 'https://i1.hdslb.com/bfs/archive/71bc7c436ef13c84706fbb15920b55e233cc76d5.jpg'
+  },
+  {
+    bvid: 'BV1p7411k7Vm',
+    title: '航拍·无锡市基督教堂',
+    publishedAt: 1580005638,
+    duration: '01:27',
+    play: 1652,
+    pic: 'https://i0.hdslb.com/bfs/archive/9976868366855717b40a6590960a8f739496d307.jpg'
+  },
+  {
+    bvid: 'BV1kJ411Q727',
+    title: 'up主中考查分时，居然......',
+    publishedAt: 1575084684,
+    duration: '03:23',
+    play: 14260,
+    pic: 'https://i0.hdslb.com/bfs/archive/1e05982187eb9147b05bce5dcaaea6543a2b610f.jpg'
+  },
+  {
+    bvid: 'BV1R4411X7N3',
+    title: '【利奇马】【无锡台风魔方赛】记2019无锡魔方公开赛主办团队遇到台风时，快速转场',
+    publishedAt: 1565492037,
+    duration: '04:01',
+    play: 256,
+    pic: 'https://i0.hdslb.com/bfs/archive/2854065f947ae2c56a4b00f206caa7c4d69829f1.jpg'
+  },
+  {
+    bvid: 'BV1K4411A7D2',
+    title: '记天一实验2016（2）班同学们毕业临行前 张老师最后的嘱托与牵挂',
+    publishedAt: 1562210158,
+    duration: '01:09',
+    play: 646,
+    pic: 'https://i1.hdslb.com/bfs/archive/58b701876020da7363d6b513c684a9904161a64e.jpg'
+  },
+  {
+    bvid: 'BV1vx411o73K',
+    title: '光謇在2019合肥赛二阶复赛场上连续加二、没起表、没停表······',
+    publishedAt: 1561376153,
+    duration: '01:27',
+    play: 300,
+    pic: 'https://i2.hdslb.com/bfs/archive/e46fe153324a4d7072606495769b88b98df00c38.jpg'
+  },
+  {
+    bvid: 'BV1f4411v7Z1',
+    title: '光謇·剪光 造型从蔡徐坤到蒋介石的蜕变',
+    publishedAt: 1556956394,
+    duration: '04:23',
+    play: 1646,
+    pic: 'https://i1.hdslb.com/bfs/archive/f2c92435c80936cef67b855bfed6aa8b4f05cc77.jpg'
+  },
+  {
+    bvid: 'BV14b411S7Eg',
+    title: '2018五地魔方联赛（无锡）光謇三速单次11.632',
+    publishedAt: 1549784615,
+    duration: '00:33',
+    play: 219,
+    pic: 'https://i1.hdslb.com/bfs/archive/b49151df0a83f93eefe8250d8c54cd608b71b7a4.jpg'
+  },
+  {
+    bvid: 'BV1Zt411p7G7',
+    title: '二班有史以来最刺激的黑板报',
+    publishedAt: 1547233557,
+    duration: '00:18',
+    play: 618,
+    pic: 'https://i1.hdslb.com/bfs/archive/0ca14eee8d8533456ab0a79ebc7421b754a157d0.jpg'
+  },
+  {
+    bvid: 'BV16t411x7FV',
+    title: '小謇教你如何处理+调试一个全新的魔方',
+    publishedAt: 1546653865,
+    duration: '21:56',
+    play: 3439,
+    pic: 'https://i1.hdslb.com/bfs/archive/70494c7758d3e294ba381e769d8118728ce9f26e.jpg'
+  },
+  {
+    bvid: 'BV1tt41167Qo',
+    title: '迎接2019，两班的新年贺词',
+    publishedAt: 1546271752,
+    duration: '04:25',
+    play: 308,
+    pic: 'https://i2.hdslb.com/bfs/archive/3a99dcd0c5e140c2e4b2dc657b5b65262205ddc0.jpg'
+  },
+  {
+    bvid: 'BV15t411i7P7',
+    title: '化学老师用心良苦的课件',
+    publishedAt: 1544787418,
+    duration: '07:39',
+    play: 639,
+    pic: 'https://i2.hdslb.com/bfs/archive/4dee4d2cea298a287dfafdfc8bb4d21e6e043de6.jpg'
+  },
+  {
+    bvid: 'BV1it411S7kv',
+    title: '【片头向】自己作为权益保障部部长开的最后一次会 2018.12.8',
+    publishedAt: 1544251196,
+    duration: '18:22',
+    play: 154,
+    pic: 'https://i1.hdslb.com/bfs/archive/950f85fec387ed31bfc69c9c9c0ee52bd80f5fd3.jpg'
+  },
+  {
+    bvid: 'BV1Ut411X7Xh',
+    title: '记2017-2018天一实验主席团最后一次例会',
+    publishedAt: 1543736295,
+    duration: '27:29',
+    play: 359,
+    pic: 'https://i2.hdslb.com/bfs/archive/4f7386c2c232414a2c9f7dcbf41fdcee9b38793e.jpg'
+  },
+  {
+    bvid: 'BV1Xt411y7Fo',
+    title: '人生中第一次比赛的第一把复原21.000',
+    publishedAt: 1543129113,
+    duration: '01:07',
+    play: 116,
+    pic: 'https://i2.hdslb.com/bfs/archive/a15321e85f65bbfbcbc390a01e823f805ed69f7f.jpg'
+  },
+  {
+    bvid: 'BV16b411w79D',
+    title: '聊聊硼处理',
+    publishedAt: 1541313458,
+    duration: '07:34',
+    play: 678,
+    pic: 'https://i1.hdslb.com/bfs/archive/avsas_i181104ws28l0d1mpsou8413aemt8dfj_0023.jpg'
+  },
+  {
+    bvid: 'BV1DW411f7hT',
+    title: '2018无锡夏季魔方公开赛 三阶速拧 决赛',
+    publishedAt: 1535772436,
+    duration: '34:51',
+    play: 639,
+    pic: 'https://i0.hdslb.com/bfs/archive/2cc54e8873d9ba281314533c3a22691d3096b510.jpg'
+  },
+  {
+    bvid: 'BV1FW411f7PS',
+    title: '2018无锡夏季魔方公开赛 二阶决赛',
+    publishedAt: 1535595232,
+    duration: '24:52',
+    play: 377,
+    pic: 'https://i2.hdslb.com/bfs/archive/a4744342803055189e0ce4565e21b68577e1643d.jpg'
+  },
+  {
+    bvid: 'BV1FW411f7FD',
+    title: '2018无锡夏季魔方公开赛 金字塔决赛',
+    publishedAt: 1535595232,
+    duration: '28:51',
+    play: 511,
+    pic: 'https://i0.hdslb.com/bfs/archive/3fe2b3b56fbc7b9a8a5e2e41ffc3861485ca3c46.jpg'
+  },
+  {
+    bvid: 'BV1ps411c7Rs',
+    title: 'Lukas Shelley在无锡夏季赛初赛的精彩瞬间',
+    publishedAt: 1535594341,
+    duration: '01:20',
+    play: 456,
+    pic: 'https://i2.hdslb.com/bfs/archive/bc7b8c90af913d7589b757de8e41c53d24a31556.jpg'
+  },
+  {
+    bvid: 'BV1uW41197rh',
+    title: '2018无锡中学足球联赛锡山区决赛完整视频',
+    publishedAt: 1534650328,
+    duration: '37:44',
+    play: 680,
+    pic: 'https://i2.hdslb.com/bfs/archive/770242c9e7259895bb237429945f29717f817491.jpg'
+  },
+  {
+    bvid: 'BV1AW411d7Mw',
+    title: '郑宇昕（光謇）第一次比车轮 二速平均4.21',
+    publishedAt: 1534513363,
+    duration: '03:29',
+    play: 135,
+    pic: 'https://i2.hdslb.com/bfs/archive/2bb25b47d47a6f80894661c730aa2698a0c365aa.jpg'
+  },
+  {
+    bvid: 'BV1zs411K78P',
+    title: '玩克-双定位v3m教程',
+    publishedAt: 1532281100,
+    duration: '23:09',
+    play: 3648,
+    pic: 'https://i0.hdslb.com/bfs/archive/860cc9071f5c48b96f2f0217f28a41c44d3e02d2.jpg'
+  },
+  {
+    bvid: 'BV1ks411H7z7',
+    title: '上海交大赛二速单次2.70',
+    publishedAt: 1531408042,
+    duration: '00:38',
+    play: 131,
+    pic: 'https://i0.hdslb.com/bfs/archive/2a792e3123cfb724aac687bac30e1d238e9e6775.jpg'
+  },
+  {
+    bvid: 'BV1ks411n7t6',
+    title: '天一实验2016级两班文艺汇演精装版视频',
+    publishedAt: 1531183497,
+    duration: '08:57',
+    play: 360,
+    pic: 'https://i1.hdslb.com/bfs/archive/7ab43eaf9df64c15e6ee0f1133ef428e90b9e425.jpg'
+  },
+  {
+    bvid: 'BV1Xs411p7Fa',
+    title: '2017无锡学联二速单次3.53',
+    publishedAt: 1531053479,
+    duration: '00:45',
+    play: 89,
+    pic: 'https://i2.hdslb.com/bfs/archive/469865761dac05fbf44aa8f91bfc7b9adc5b7845.jpg'
+  },
+  {
+    bvid: 'BV1Es411V76N',
+    title: '用国甲比wca的骚操作',
+    publishedAt: 1530697175,
+    duration: '00:53',
+    play: 262,
+    pic: 'https://i0.hdslb.com/bfs/archive/e457b8951c7cde65abdc3010c09090dbc16c9450.jpg'
+  },
+  {
+    bvid: 'BV1cs411V7zL',
+    title: '2018上海交大赛四速单次57.691',
+    publishedAt: 1530645036,
+    duration: '01:18',
+    play: 73,
+    pic: 'https://i1.hdslb.com/bfs/archive/07fb4bcd401489e0a5d154b5d3252d07059cfb2a.jpg'
+  },
+  {
+    bvid: 'BV1fs411G71d',
+    title: '2018无锡市锡山区校园足球联赛决赛 仓下2：0天一 部分视频',
+    publishedAt: 1530644470,
+    duration: '07:02',
+    play: 695,
+    pic: 'https://i2.hdslb.com/bfs/archive/a854aab686fc04abe63eaa2db1923db534f18bb3.jpg'
+  }
+];
+
+export function getBilibiliUrl(bvid: string) {
+  return `https://www.bilibili.com/video/${bvid}`;
+}
+
+export function getBilibiliEmbedUrl(bvid: string) {
+  return `https://player.bilibili.com/player.html?bvid=${bvid}&page=1&high_quality=1&danmaku=0`;
+}
